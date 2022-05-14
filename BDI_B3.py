388e3ba3dcb47d5ce584dec75ded98a49e5a8b2f
jovepy <datajove@gmail.com>
2022-05-14 11:01:49 -0300

    Create BDI_B3.py

---
+++ BDI_B3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 14 10:50:13 2022
+
+@author: rodrigo.jove
+"""
+
+import requests
+from datetime import date, timedelta
+import pandas as pd
+import numpy as np
+
