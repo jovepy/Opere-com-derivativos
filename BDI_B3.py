6bbf8e073ab698c3a52520a20dd964880501aa3c
Rodrigo Jovê <datajove@gmail.com>
2022-06-03 22:02:28 -0300

    dados estruturados

--- BDI_B3.py
+++ BDI_B3.py
@@ -26,6 +26,49 @@ if aux == 200:
         filename = Path(r'temp\BDI_DIA.pdf')
         filename.write_bytes(r.content)
         
-dfs = tabula.read_pdf("temp\BDI_DIA.pdf", pages='all')
+dfs = tabula.read_pdf("temp\BDI_DIA.pdf", pages='all', pandas_options=({'header': None}))
 
-dfs[0]
+
+df = pd.DataFrame()
+for i in dfs:
+    df = pd.concat([df,i],axis=0)
+
+df1 = pd.DataFrame()
+for coluna in df:
+    df2 = pd.DataFrame()
+    aux= pd.DataFrame(df[coluna].str.split(' ',expand=True))
+    aux.columns = list(range(coluna,coluna+len(aux.columns)))
+    df1 = pd.concat([df1,aux],axis=1)
+    
+df1.to_excel('ver.xlsx')
+df1 = df1.set_index(0)
+
+tickers = []
+vencimentos = [] 
+for ticker in df1.index:
+    for i in df1.loc[ticker]:
+        try:
+            ano = date.today().strftime("%Y")
+            if ano in i:
+                for n in list(range(len(i))):
+                    if i[n:n+(len('/{}'.format(ano)))] == '/{}'.format(ano):
+                        vencimentos.append(i[len('xx/xx/xxxx')*-1:n+len('/{}'.format(ano))])
+                        tickers.append(ticker)
+        except Exception as e:
+            pass
+
+
+
+df_estruturado = pd.DataFrame()
+for ticker, vencimento in zip(tickers,vencimentos):
+    aux = (list(df1.loc[ticker].dropna()))[-11:] 
+    while ano in aux[0]:
+        aux=aux[1:]
+        aux[-1] = '-'
+    aux = pd.DataFrame([ticker,vencimento]+aux).T
+    df_estruturado = pd.concat([df_estruturado,aux],axis=0)
+    
+df_estruturado = df_estruturado[df_estruturado.columns[:-2]] #exclui-se as colunas de negócios realizados, pois não quero nenhum viés de mercado
+
+    
+df_estruturado.columns = ['Código' ,'Vencimento' ,'Exercício','Abertura' ,'Mínimo','Máximo', 'Médio' ,'Fechamento', 'Oscilação (%)','Compra (R$)' ,'Venda (R$)']
